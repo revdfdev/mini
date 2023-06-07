c52c99f3f22e79dfbe06c4b3ce09c702a4ca8162
revdfdev <mkodekar@zoho.com>
2023-06-08 00:23:03 +0530

    Print routes with handlers

--- router.go
+++ router.go
@@ -1,5 +1,10 @@
 package mini
 
+import (
+	"reflect"
+	"runtime"
+)
+
 type Route struct {
 	Path       string
 	Method     string
@@ -10,3 +15,8 @@ type Route struct {
 type HandlerFunc func(*Context) error
 
 type MiddlewareFunc func(HandlerFunc) HandlerFunc
+
+func (handler *HandlerFunc) Name(i interface{}) string {
+	funcName := runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
+	return funcName
+}
